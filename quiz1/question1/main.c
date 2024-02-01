1e18560330019101fde6512ffbeb21c630b3e70a
Lincoln Muraguri <lincolnmuraguri93@gmail.com>
2024-02-01 10:04:42 +0300

    question 1 completed

--- quiz1/question1/main.c
+++ quiz1/question1/main.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 
-int main(int a, int b, int c){
-    printf("Enter the first integer:\n");
-    scanf("%d",&a);
-    printf("Enter the second integer");
-    scanf("%d",&b);
-    c = a+b;
-    printf("The result is %d",c);
+int main() {
+    int arr[10] = {2, 3, 4, 2, 7, 8, 4, 1, 9, 6}; 
+    int i, j, k, size;
+
+    size = sizeof(arr) / sizeof(arr[0]);
+    printf("size of arr: %d\n", size);
+
+    for (i = 0; i < size; i++) {
+
+        for (j = i + 1; j < size;) {
+
+            if (arr[i] == arr[j]) {
+
+                for (k = j; k < size - 1; k++) {
+
+                    arr[k] = arr[k + 1];
+
+                }
+
+                size--;
+
+            } else {
+
+                j++;
+
+            }
+        }
+    }
+
+    printf("Array elements after deletion of duplicate elements: ");
+    for (i = 0; i < size; i++) {
+        printf("%d\t", arr[i]);
+    }
+
     return 0;
 }
