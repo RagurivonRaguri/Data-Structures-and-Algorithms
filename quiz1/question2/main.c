73e13bd68ca1372786f8784fc71dd68991307f32
Lincoln Muraguri <lincolnmuraguri93@gmail.com>
2024-02-01 12:05:35 +0300

    completed question 2

---
+++ quiz1/question2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int i, j, k;
+    int size_of_arr = sizeof(arr) / sizeof(arr[0]);
+    printf("%d\n", size_of_arr);
+
+    for (i=0, j=9; i<j; i++, j--) {
+        k = arr[i];
+        arr[i] = arr[j];
+        arr[j] = k;
+    }
+
+    printf("The rotated array:\n");
+    for (i = 0; i < size_of_arr; i++) {
+        printf("%d \n", arr[i]);
+    }
+
+    return 0;
+}
