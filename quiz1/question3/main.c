b2db8e9d7656381f28bfeadaf9991e9eab6ef36d
Isaiah Onando Mulang <mulang.onando@gmail.com>
2024-02-05 13:34:31 +0300

    Update main.c

--- quiz1/question3/main.c
+++ quiz1/question3/main.c
@@ -1,6 +1,13 @@
+/*
+It's good to include here a comment containing the question you are solving and a description of your proposed solution
+    */
+
 #include <stdio.h>
 #include <stdbool.h>
 
+/*
+    Duplicates are not only consecutive numbers. Test this code with this array --> [29,2,4,5,29,2,11,15,60,29]
+    */
 int main(){
     int arr[10]= {0,0,1,2,5,2,3,3,4,6};
     int size = sizeof(arr)/sizeof(arr[0]);
