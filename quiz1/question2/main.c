1091a14ce2724205a4e4be94d7c24393fb4eefd1
Lincoln Muraguri <123552224+RagurivonRaguri@users.noreply.github.com>
2024-02-17 12:55:50 +0300

    Updated main.c

--- quiz1/question2/main.c
+++ quiz1/question2/main.c
@@ -1,27 +1,38 @@
 /*
-It's good to include here a comment containing the question you are solving and a description of your proposed solution
-    */
-/*
-    Not sure if this solution rotates
+Question Two (2) - Rotate Array
+Given an array, rotate the array to the right by k steps, where k is non-negative.
 */
 #include <stdio.h>
 
+void rotateArray(int arr[], int size) {
+    int temporary_element = arr[size - 1]; // Stores the last element temporarily
+
+    // Shift elements to the right by one position
+    for (int i = size - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = temporary_element; // Move the last element to the first position
+}
+
 int main() {
     int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int i, j, k;
     int size_of_arr = sizeof(arr) / sizeof(arr[0]);
-    printf("%d\n", size_of_arr);
 
-    for (i=0, j=9; i<j; i++, j--) {
-        k = arr[i];
-        arr[i] = arr[j];
-        arr[j] = k;
+    printf("Original array:\n");
+    for (int i = 0; i < size_of_arr; i++) {
+        printf("%d ", arr[i]);
     }
+    printf("\n");
+
+    rotateArray(arr, size_of_arr);
 
     printf("The rotated array:\n");
-    for (i = 0; i < size_of_arr; i++) {
-        printf("%d \n", arr[i]);
+    for (int i = 0; i < size_of_arr; i++) {
+        printf("%d ", arr[i]);
     }
+    printf("\n");
 
     return 0;
 }
+
